026785efcc87f40780f4c7abee48fd3e768a7134
Neri Yarden <neriyarden@gmail.comgit add git add --allgit statusgit config --global user.email neriyarden@gmail.com>
2021-09-24 22:57:57 +0300

    error fallback improvements

--- src/services/errors/ErrorFallback.jsx
+++ src/services/errors/ErrorFallback.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRedoAlt } from '@fortawesome/free-solid-svg-icons'
 
 function ErrorFallback({ errorMsg }) {
+	const refreshPage = () => {
+		window.location.reload(false)
+	}
 	return (
 		<div role='alert' className={'error-fallback'}>
-			<h1>404 😕</h1>
+			<h1>oops.. 😕</h1>
 			<h2>Something went wrong.</h2>
 			<pre>{errorMsg}</pre>
 			<p>
-				Sorry for the inconvenience, The admin has been notified about
-				this issue. Please try again later.
+				Sorry for the inconvenience, Our team has been notified about
+				this and will fix the problem ASAP.
 			</p>
+			<p>Please try again later.</p>
+			<button className='refresh-btn' onClick={refreshPage}>
+				<FontAwesomeIcon icon={faRedoAlt} />
+			</button>
 		</div>
 	)
 }
